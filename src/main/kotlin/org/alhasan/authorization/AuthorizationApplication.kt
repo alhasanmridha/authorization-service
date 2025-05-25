6adb078d0d61b09aa1ed808fa7f712ff6f7c92b5
Al-Hasan Mridha <mdalhasanmridha@gmail.com>
2025-05-26 08:56:22 +0900

    init

---
+++ src/main/kotlin/org/alhasan/authorization/AuthorizationApplication.kt
@@ -0,0 +1,11 @@
+package org.alhasan.authorization
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class AuthorizationApplication
+
+fun main(args: Array<String>) {
+	runApplication<AuthorizationApplication>(*args)
+}
